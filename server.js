0124e1b16c84219e19c0bc24b727fba3bed73271
shalabyelectronics <mhdshalabystar@gmail.com>
2022-10-26 15:52:15 +0300

    Add post and get request to servery

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('/',(req,res)=>{
     res.sendFile('index.html')
 })
 
+app.post('/',(req,res)=>{
+    const weight = Number(req.body.weight);
+    const height = Number(req.body.height);
+    const bmiResult = (weight/(height**2)).toFixed(2);
+
+    res.send(`The BMI Result is ${bmiResult}`);
+})
+
 app.listen(port,()=>{
     console.log(`Server Runing on port ${port}`)
 })
